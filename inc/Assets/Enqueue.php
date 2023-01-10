3fa50dac7b5ad8877b64175064f3d7165d551624
shewa <30765517+shewa12@users.noreply.github.com>
2023-01-10 18:09:44 +0600

    New: Prefix/Slug replace method added

--- inc/Assets/Enqueue.php
+++ inc/Assets/Enqueue.php
@@ -33,7 +33,7 @@ class Enqueue {
 		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'script_text_domain' ) );
 
 		// WP Cli command.
-		add_action( 'init', array( __CLASS__, 'register_cli_command' ) );
+		add_action( 'cli_init', array( __CLASS__, 'register_cli_command' ) );
 	}
 
 	/**
@@ -48,12 +48,12 @@ class Enqueue {
 		$minify      = 'DEV' === $plugin_data['env'] ? '' : 'min.';
 		// load styles & scripts only required page.
 
-		wp_enqueue_style( 'plugin-starter-backend-style', $plugin_data['assets'] . 'css/ps-backend.css', array(), filemtime( $plugin_data['plugin_path'] . "assets/css/ps-backend.{$minify}css" ) );
+		wp_enqueue_style( 'some-plugin-backend-style', $plugin_data['assets'] . 'css/ps-backend.css', array(), filemtime( $plugin_data['plugin_path'] . "assets/css/ps-backend.{$minify}css" ) );
 
-		wp_enqueue_script( 'plugin-starter-backend', $plugin_data['assets'] . 'js/backend/ps-backend.js', array( 'wp-i18n' ), filemtime( $plugin_data['plugin_path'] . "assets/js/backend/ps-backend{$minify}.js" ), true );
+		wp_enqueue_script( 'some-plugin-backend', $plugin_data['assets'] . 'js/backend/ps-backend.js', array( 'wp-i18n' ), filemtime( $plugin_data['plugin_path'] . "assets/js/backend/ps-backend{$minify}.js" ), true );
 
 		// add data to use in js files.
-		wp_add_inline_script( 'plugin-starter-backend', 'const awData = ' . json_encode( self::scripts_data() ), 'before' );
+		wp_add_inline_script( 'some-plugin-backend', 'const awData = ' . json_encode( self::scripts_data() ), 'before' );
 	}
 
 	/**
@@ -93,26 +93,75 @@ class Enqueue {
 	 */
 	public static function script_text_domain() {
 		$plugin_data = PluginStarter::plugin_data();
-		wp_set_script_translations( 'plugin-starter-backend', $plugin_data['plugin_path'] . 'assets/languages/' );
+		wp_set_script_translations( 'some-plugin-backend', $plugin_data['plugin_path'] . 'assets/languages/' );
 	}
 
+	/**
+	 * Update plugin slug with CLI command
+	 *
+	 * @since 1.0.0
+	 *
+	 * @return void
+	 */
 	public static function register_cli_command() {
 		\WP_CLI::add_command(
-			'plugin-starter replace-slug',
+			'ps replace-slug',
 			function( $args ) {
 				if ( is_array( $args ) && count( $args ) ) {
 					$slug = $args[0];
 					if ( $slug ) {
+						self::update_slug( $slug );
 						\WP_CLI::success( $slug );
 					} else {
 						\WP_CLI::error( 'Invalid slug' );
 					}
 				} else {
 					\WP_CLI::warning( 'Slug argument is missing' );
-					\WP_CLI::line( 'Example command: wp plugin-starter replace_slug some-slug' );
+					\WP_CLI::line( 'Example command: wp some-plugin replace_slug some-slug' );
 				}
 			}
 		);
 	}
 
+	/**
+	 * Update default some-plugin slug
+	 *
+	 * Search slug/prefix on each file and replace with
+	 * the given slug
+	 *
+	 * @since 1.0.0
+	 *
+	 * @param string $slug update to this slug.
+	 * @param string $dir directory path.
+	 *
+	 * @return bool
+	 */
+	private static function update_slug( $slug, $dir = '' ) {
+		$plugin_data = PluginStarter::plugin_data();
+		$dir         = '' === $dir ? $plugin_data['plugin_path'] : $dir;
+
+		$needles = array( 'some-plugin', 'some-plugin' );
+		$files   = scandir( $dir );
+		if ( is_array( $files ) && count( $files ) ) {
+			foreach ( $files as $file ) {
+				// If directory then get back & scan files again.
+				if ( ! in_array( $file, array( '.', '..' ) ) && is_dir( $dir . '/' . $file ) ) {
+					$child_dir = $dir . DIRECTORY_SEPARATOR . $file;
+					self::update_slug( $slug, $child_dir );
+				} else {
+
+					if ( ! in_array( $file, array( '.', '..' ) ) ) {
+						$abs_file_path = trailingslashit( $dir ) . $file;
+						$file_content  = file_get_contents( $abs_file_path, true );
+
+						foreach ( $needles as $needle ) {
+							$file_content = str_replace( $needle, $slug, $file_content );
+							file_put_contents( $abs_file_path, $file_content );
+						}
+					}
+				}
+			}
+		}
+		return true;
+	}
 }
