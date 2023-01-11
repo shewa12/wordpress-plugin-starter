426e3655bd3a0f9b3ff93242ef2d9044394aa53f
shewa12 <shewa12kpi@gmail.com>
2023-01-11 17:44:30 +0600

    Update: Log message updated

--- assets/js/backend/ps-backend.js
+++ assets/js/backend/ps-backend.js
@@ -7,5 +7,5 @@
  */
 const { __ } = wp.i18n;
 document.addEventListener("DOMContentLoaded", function () {
-console.log(__('Hi there', 'plugin-starter'));
+console.log(__('Hello WordPress Plugin Starter User', 'plugin-starter'));
 });
